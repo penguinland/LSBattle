440735ca0a9971486375544f251165d79d1fdce3
Alan Davidson <alan.davidson@gmail.com>
2024-11-29 01:49:56 -0500

    clean up LSBattle3D.py: refactor duplicated code to helper function, import some things at the top

--- LSBattle3D.py
+++ LSBattle3D.py
@@ -1,13 +1,9 @@
-# coding: utf8
-# LSBattle.py
-try:
-    import OpenGL
-    OpenGL.ERROR_CHECKING = False
-    # OpenGL.STORE_POINTERS = False
-    # OpenGL.UNSIGNED_BYTE_IMAGES_AS_STRING = False
-    import program.const
-except:
-    import sys, traceback, os
+#!/usr/bin/env python3
+import os
+import sys
+import traceback
+
+def log_error():
     exc_type, exc_value, exc_traceback = sys.exc_info()
     if os.name == "posix":
         p = os.path.dirname(sys.argv[0]) # app/Contants/MacOS
@@ -20,6 +16,15 @@ except:
     traceback.print_exception(exc_type, exc_value, exc_traceback,
                               file=open(p, "w")
                               )
+
+try:
+    import OpenGL
+    OpenGL.ERROR_CHECKING = False
+    # OpenGL.STORE_POINTERS = False
+    # OpenGL.UNSIGNED_BYTE_IMAGES_AS_STRING = False
+    import program.const
+except:
+    log_error()
     sys.exit(1)
 
 def run():
@@ -31,18 +36,6 @@ if __name__ == "__main__":
     try:
         run()
     except Exception:
-        import sys, traceback, os
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        if os.name == "posix":
-            p = os.path.dirname(sys.argv[0]) # app/Contants/MacOS
-            p = os.path.dirname(p) # app/Contants/
-            p = os.path.dirname(p) # app/
-            p = os.path.dirname(p)
-        else:
-            p = os.path.dirname(sys.argv[0])
-        p = os.path.join(p, "error.log")
-        traceback.print_exception(exc_type, exc_value, exc_traceback,
-                                  file=open(p, "w")
-                                  )
+        log_error()
         from program.box import BOX
         BOX.game_quit()
