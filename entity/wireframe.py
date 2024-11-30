3d84e3244bd2090c2a03af5aa236461fe37a89c7
Alan Davidson <alan.davidson@gmail.com>
2024-11-30 16:44:32 -0500

    whitespace and other minor tweaks

--- entity/wireframe.py
+++ entity/wireframe.py
@@ -17,13 +17,15 @@ class WireFrame(object):
 
     @classmethod
     def make_lattice(cls, N, L):
-        if cls.vertices is not None:
+        if cls.vertices is not None:  # Already done previously
             return
+
         def add(xx, yy, zz, a):
             vertices.extend([xx, yy, zz])
             if a < N * c:
-                indices.append(len(vertices) // 3 -1)
+                indices.append(len(vertices) // 3 - 1)
                 indices.append(len(vertices) // 3)
+
         c = script.world.wireframe.inner_div
         vertices = []
         indices = []
