738bc95fc34bcf0cb9160a6589dd9540f818aac9
Alan Davidson <alan.davidson@gmail.com>
2024-12-01 00:04:19 -0500

    whitespace tweaks, rename variables to be clearer, import only the things we use

--- sequence/gameover.py
+++ sequence/gameover.py
@@ -3,21 +3,22 @@ import sdl2
 
 from program import script
 from program.box import BOX
-from program.const import *
+from program.const import KS_ESC, KS_RETURN
 from sequence.locals import MenuItems
 
 
 class GameOver(object):
     def __init__(self):
-        self.text = ["Game Over"]
-        self.text2 = ["Game Clear!!"]
+        self.lose_text = ["Game Over"]
+        self.win_text = ["Game Clear!!"]
 
     def init(self, text):
-        self.menu = MenuItems(text, BOX.Y/8, 0.4)
+        self.menu = MenuItems(text, BOX.Y / 8, 0.4)
 
-    def mainloop(self, flg=True):
-        self.init(self.text if flg else self.text2)
+    def mainloop(self, won=True):
+        self.init(self.win_text if won else self.lose_text)
         start = sdl2.SDL_GetTicks()
+
         while True:
             for event in sdl2.ext.get_events():
                 if event.type == sdl2.SDL_QUIT:
